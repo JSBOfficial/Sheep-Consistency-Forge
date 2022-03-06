70cdc50f9849eef7ca3480fe9e0622717e500351
JSBOfficial <JSBOfficialMail@gmail.com>
2022-03-06 20:04:55 +0800

    Ported to 1.18.2 Complete
    I didn't push every changes in.

---
+++ src/main/java/me/jsb/sheepconsistencyforge/SheepConsistencyForge.java
@@ -0,0 +1,13 @@
+package me.jsb.sheepconsistencyforge;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SheepConsistencyForge.MODID)
+public class SheepConsistencyForge {
+	public static final String MODID = "sheepconsistencyforge";
+
+	public static final Logger LOGGER = LogManager.getLogger(SheepConsistencyForge.MODID);
+}
